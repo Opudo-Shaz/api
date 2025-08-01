3864f41a017c64f5fd6efd1a7f4b99d7f068ad7a
Opudo-Shaz <opudosharon6@gmail.com>
2025-08-01 10:53:24 +0300

    initiallized react+vite for UI and Logic

---
+++ testingfile.js
@@ -0,0 +1,23 @@
+var x = 14;
+var y = 28;
+var z = x + y;
+console.log("The sum of x and y is: " + z);
+
+let n = 42;
+console.log(`The value of n is: ${n}`);
+
+
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+
+console.log(greet("Alice"));
+console.log(greet("Bob"));
+
+
+function concatenateStrings(str1, str2) {
+    return str1 + " " + str2;
+}
+
+console.log(concatenateStrings("Hello", "World"));
+console.log(concatenateStrings("Goodbye", "Everyone"));
